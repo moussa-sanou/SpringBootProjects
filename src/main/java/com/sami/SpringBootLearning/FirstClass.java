0f014c3865ffe1d1984dd0dd760970aa8f7fa5ed
Sami Dollars <muss.junior@gmail.com>
2024-06-18 22:28:24 -0500

    Spring beans in action

---
+++ src/main/java/com/sami/SpringBootLearning/FirstClass.java
@@ -0,0 +1,10 @@
+package com.sami.SpringBootLearning;
+
+public class FirstClass {
+
+    public String sayHello()
+    {
+        return "Hello from the first class";
+    }
+
+}
--- src/main/java/com/sami/SpringBootLearning/SpringBootLearningApplication.java
+++ src/main/java/com/sami/SpringBootLearning/SpringBootLearningApplication.java
@@ -2,12 +2,23 @@ package com.sami.SpringBootLearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootLearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLearningApplication.class, args);
+		var ctx = SpringApplication.run(SpringBootLearningApplication.class, args);
+
+		FirstClass firstClass = ctx.getBean(FirstClass.class);
+		String ans = firstClass.sayHello();
+		System.out.println(ans);
+	}
+
+	@Bean
+	public FirstClass firstClass()
+	{
+		return new FirstClass();
 	}
 
 }
