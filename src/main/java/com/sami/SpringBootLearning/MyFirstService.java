6ec4c4a8e03fd4ea4170c2d9b69481fc883900e4
Sami Dollars <muss.junior@gmail.com>
2024-07-01 23:02:21 -0500

    Day 1 of the 100 days challenge

--- src/main/java/com/sami/SpringBootLearning/MyFirstService.java
+++ src/main/java/com/sami/SpringBootLearning/MyFirstService.java
@@ -1,11 +1,15 @@
 package com.sami.SpringBootLearning;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
-
     private FirstClass firstClass;
+    @Autowired
+    public MyFirstService(FirstClass firstClass) {
+        this.firstClass = firstClass;
+    }
 
     public String tellAStory()
     {
